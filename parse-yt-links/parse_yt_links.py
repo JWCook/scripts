7d5c61035897f9ac24a109e10357502020943abd
Jordan Cook <jordan.cook.git@proton.me>
2024-01-16 19:51:20 -0600

    Add basic command line options

--- parse-yt-links/parse_yt_links.py
+++ parse-yt-links/parse_yt_links.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
+import argparse
 import re
-import sys
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 from html import unescape
+from logging import basicConfig, getLogger
 from operator import attrgetter
 from pathlib import Path
 from urllib.parse import unquote
@@ -11,6 +12,7 @@ from urllib.parse import unquote
 from requests_cache import CachedSession
 from rich.progress import track
 
+logger = getLogger(__name__)
 session = CachedSession('yt_pages.db', expire_after=timedelta(hours=1))
 
 
@@ -39,22 +41,31 @@ class Video:
         return f'[{self.date_str}] [{self.views:,}] {self.title} ({self.url})'
 
 
-def parse_videos(source: Path | str, sort_col: str = 'date') -> list[Video]:
+def parse_videos(
+    input_file: str | str, sort_col: str = 'date', ascending: bool = False
+) -> list[Video]:
     """Get Video objects from a text file containing YouTube URLs"""
-    urls = _get_yt_urls(source)
+    urls = _get_yt_urls(input_file)
     videos = []
-    for url in track(urls, description='Parsing videos...'):
+    for url in track(urls, description='Fetching video metadata...'):
         videos.append(Video.from_url(url))
-    return sorted(videos, key=attrgetter(sort_col), reverse=True)
+
+    logger.debug(f'Found {len(videos)} video links in {input_file}')
+    return sorted(videos, key=attrgetter(sort_col), reverse=not ascending)
 
 
-def to_md(videos: list[Video], dest: str | Path = None, table: bool = False) -> str:
+def to_md(videos: list[Video], output_file: str = None, table: bool = False) -> str:
     """Convert a list of Video objects to a Markdown list or table"""
     md = f'# Videos ({len(videos)})\n\n'
     md += _md_table(videos) if table else _md_list(videos)
 
-    if dest:
-        with Path(dest).open('w') as f:
+    if not videos:
+        logger.warning('No videos found')
+        return md
+
+    if output_file:
+        logger.debug(f"Writing {'table' if table else 'list'} to {output_file}")
+        with Path(output_file).open('w') as f:
             f.write(md)
     return md
 
@@ -113,24 +124,70 @@ def _get_date(html: str) -> datetime:
     return datetime.strptime(date_str, '%b %d, %Y')
 
 
-def _get_yt_urls(source: Path | str):
+def _get_yt_urls(input_file: str):
     """Extract any youtube URLs from a text file or string"""
-    if isinstance(source, Path):
-        source = source.read_text()
+    content = Path(input_file).read_text()
 
     # Too lazy to get this regex working
     # return re.findall(r'https?://(?:www\.)?(?:youtu\.be/|youtube\.com\S+)', text)
 
-    urls = re.findall(r'(https?://[^\s^\)]+)', source)
+    urls = re.findall(r'(https?://[^\s^\)]+)', content)
     return [url for url in urls if 'youtube.com' in url or 'youtu.be' in url]
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1 or sys.argv[1] in ('-h', '--help'):
-        print(f'Usage: {sys.argv[0]} [path]')
-        sys.exit(0)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', help='Path to text file containing YouTube URLs')
+    parser.add_argument(
+        '-i',
+        '--inplace',
+        action='store_true',
+        help='Overwrite input file',
+    )
+    parser.add_argument(
+        '-o',
+        '--output_file',
+        help='Output file',
+    )
+    parser.add_argument(
+        '-r',
+        '--reverse',
+        action='store_true',
+        help='Reverse sort order (ascending)',
+    )
+    parser.add_argument(
+        '-s',
+        '--sort',
+        choices=['date', 'title', 'views'],
+        default='date',
+        help='Sort order for output',
+    )
+    parser.add_argument(
+        '-t',
+        '--table',
+        action='store_true',
+        help='Output a Markdown table instead of list',
+    )
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help='Enable verbose logging',
+    )
+    args = parser.parse_args()
 
-    videos = parse_videos(Path(sys.argv[1]))
-    # to_md(videos, Path('table.md'), table=True)
-    for video in videos:
-        print(video)
+    if args.verbose:
+        basicConfig(level='INFO')
+        logger.setLevel('DEBUG')
+
+    videos = parse_videos(args.input_file, sort_col=args.sort, ascending=args.reverse)
+    output_file = args.input_file if args.inplace else args.output_file
+    to_md(videos, output_file, table=args.table)
+
+    if not output_file or args.verbose:
+        for video in videos:
+            print(video)
+
+
+if __name__ == '__main__':
+    main()
