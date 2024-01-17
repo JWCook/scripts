97e159e87b706b517e1eec39614d39600edf515a
Jordan Cook <jordan.cook.git@proton.me>
2024-01-16 19:57:18 -0600

    Add audio Bible tagger

---
+++ audio-bible-tagger/README.md
@@ -0,0 +1,7 @@
+# Audio Bible Tagger
+A script to add book/chapter metadata and file names for an unlabeled set of audio Bible files
+(track 1, ...1189).
+
+Used for the following album in MP4 format:
+Voice Only Audio Bible - New King James Version, NKJV (Narrated by Tinasha LaRayé)
+ISBN 9780785229780
---
+++ audio-bible-tagger/rename_tracks.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import csv
+from logging import basicConfig, getLogger
+from pathlib import Path
+from typing import Iterable
+
+from mutagen.mp4 import MP4
+
+logger = getLogger(__name__)
+basicConfig(level='DEBUG')
+BookChapters = list[tuple[str, int]]
+
+AUDIO_DIR = Path('/mnt/d/audio_bible')
+
+
+def get_book_chapters() -> BookChapters:
+    with open('books.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        book_n_chapters = {row[0]: int(row[1]) for row in reader}
+
+    book_chapters = []
+    for book, n_chapters in book_n_chapters.items():
+        for n in range(n_chapters):
+            # book_chapters.append(f'{book} Chapter {n+1}')
+            book_chapters.append((book, n + 1))
+
+    return book_chapters
+
+
+def write_track_name(path: Path, book_chapters: BookChapters):
+    track = MP4(path)
+
+    # Find book and chapter based on track number
+    idx = track.tags['trkn']
+    while isinstance(idx, Iterable):
+        idx = idx[0]
+    book, chapter = book_chapters[idx - 1]
+    title = f'{book} Chapter {chapter}'
+
+    # Write metadata
+    track.tags['©nam'] = title
+    track.tags['©grp'] = book
+    track.save()
+
+    # Rename
+    new_path = path.parent / f'{idx:0>4} - {title}.m4a'
+    logger.info(f'Moving {path.name} to {new_path.name}')
+    path.rename(new_path)
+
+
+def main():
+    book_chapters = get_book_chapters()
+    logger.info(f'Found {len(book_chapters)} track names')
+    paths = list(AUDIO_DIR.glob('*'))
+    logger.info(f'Found {len(paths)} audio files')
+    for track_path in paths:
+        write_track_name(track_path, book_chapters)
+
+
+if __name__ == '__main__':
+    main()
